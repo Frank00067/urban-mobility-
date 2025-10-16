50fc6958f5314b7d7d41ba9eb8fc44f6819316ac
Gasasiragrace <g.umwari@alustudent.com>
2025-10-16 23:28:37 +0200

    Initialize Supabase client with environment variables
    finished

---
+++ src/utils/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import "dotenv/config";
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SECRET_KEY) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET_KEY,
+);
+
+export default supabase;
